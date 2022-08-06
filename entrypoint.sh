57419060d28696dc3271d6a845f312e50c09859a
Anton Novikau <anton@talaera.com>
2022-08-05 20:12:39 -0700

    feat: Added public keys support; refactor.

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,13 +11,14 @@ eval $(ssh-agent)
 ssh-add "$HOME/.ssh/docker"
 
 # Add public key to known hosts.
-printf '%s %s\n' "$SSH_HOST" "$INPUT_SSH_PUBLIC_KEY" >> /etc/ssh/ssh_known_hosts
+printf '%s %s\n' "$INPUT_SSH_HOST" "$INPUT_SSH_PUBLIC_KEY" >> /etc/ssh/ssh_known_hosts
+printf '%s %s\n' "$INPUT_SSH_PROXY_HOST" "$INPUT_SSH_PROXY_PUBLIC_KEY" >> /etc/ssh/ssh_known_hosts
 
 # Put correct values into ssh config file
 sed -i -e 's/%user%/'"$INPUT_SSH_USER"'/g' $HOME/.ssh/config
 sed -i -e 's/%hostname%/'"$INPUT_SSH_HOST"'/g' $HOME/.ssh/config
 sed -i -e 's/%proxy_user%/'"$INPUT_SSH_PROXY_USER"'/g' $HOME/.ssh/config
-sed -i -e 's/%proxy_hostname%/'"$INPUT_SSH_PROXY_HOSTNAME"'/g' $HOME/.ssh/config
+sed -i -e 's/%proxy_hostname%/'"$INPUT_SSH_PROXY_HOST"'/g' $HOME/.ssh/config
 
 echo "Connecting to $INPUT_SSH_HOST..."
 docker --log-level debug --host "ssh://docker-server" "$@" 2>&1
